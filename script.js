89e8a388d4d14c481407ede7133bccade5b77057
Yesica <yesicamg18@hotmail.com>
2020-09-17 18:25:44 -0500

    feat:change general html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   $(".twentytwenty-container").twentytwenty({
     default_offset_pct: 0.5, // How much of the before image is visible when the page loads
     orientation: "horizontal", // Orientation of the before and after images ('horizontal' or 'vertical')
@@ -7,12 +7,12 @@ $(function () {
     no_overlay: false, //Do not show the overlay with before and after
     move_slider_on_hover: true, // Move slider on mouse hover?
     move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement.
-    click_to_move: false, // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+    click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
   });
 });
 var boton = document.getElementById("botonModal");
 
-window.onscroll = function () {
+window.onscroll = function() {
   console.log(document.documentElement.scrollTop);
   if (document.documentElement.scrollTop >= 300) {
     boton.classList.add("activo");
@@ -20,6 +20,6 @@ window.onscroll = function () {
     boton.classList.remove("activo");
   }
 };
-$("#myModal").on("shown.bs.modal", function () {
+$("#myModal").on("shown.bs.modal", function() {
   $("#myInput").trigger("focus");
 });
